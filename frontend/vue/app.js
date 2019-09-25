b74aa69e73987cc8301d6a4202733100df75fb72
Mark Salisbury <fmark.salisbury@gmail.com>
2019-09-25 15:54:58 -0600

    Tweak code...

--- frontend/vue/app.js
+++ frontend/vue/app.js
@@ -3,6 +3,12 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+		activeMode: 'Add',
+		modes: ['Add', 'View'],
+		activeArea: 'location',
+		locations: [],
+		selectedLocation: null,
+		creatingNewLocation: false,
 		product: 'Socks',
 		image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
 		inStock: false,
@@ -19,11 +25,32 @@ var app = new Vue({
 		cart: 0
 	},
 	methods: {
+		switchMode(newMode) {
+			this.activeMode = newMode
+		},
+		clickNewLocation() {
+			this.creatingNewLocation = true
+		},
+		clickAddNewLocation() {
+			var location_name = document.getElementById('new-location-name');
+			if (location_name.value) {
+				var newLocation = {
+					name: location_name.value
+				};
+				this.locations.push(newLocation);
+				this.switchLocation(newLocation);
+			}
+			this.creatingNewLocation = false
+		},
+		switchLocation(location) {
+			this.selectedLocation = location;
+		},
 		addToCart() {
 			this.cart += 1
 		},
 		updateProduct(variantImage) {
 			this.image = variantImage
-		}
+		},
+
 	}
 })
